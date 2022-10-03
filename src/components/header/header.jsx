c54672adce6f2c9ca7640e1f9ceaeb01e75bcafe
LabCap <banlazthosow@gmail.com>
2022-10-04 00:20:55 +0300

    created 'header'

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./header.scss";
+
+export const Header = () => {
+	return (
+		<header className="header">
+			<div className="container">
+				<div className="ico-logo"></div>
+			</div>
+		</header>
+	);
+};
