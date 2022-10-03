642c475ad0af0b641bf88e5b8e2447c43e8afe15
LabCap <banlazthosow@gmail.com>
2022-10-04 00:21:52 +0300

    created 'posts/post'

---
+++ src/components/post/post.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./post.scss";
+
+export const Post = () => {
+	return (
+		<div className="post">
+			<div className="post__img"></div>
+			<div className="post__text">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Blanditiis, vero? Sint, expedita illo quaerat facilis nulla
+				provident delectus molestiae harum, sapiente quidem dolorem?
+				Adipisci, aliquid quia? Animi consequuntur expedita molestiae?
+			</div>
+		</div>
+	);
+};
---
+++ src/components/posts/posts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Post } from "../post/post";
+
+import "./posts.scss";
+
+export const Posts = () => {
+	return (
+		<div className="posts">
+			<Post />
+		</div>
+	);
+};
