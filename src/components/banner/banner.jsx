21daf6e2ea6c198a61b297fe9277d11cfa98e373
LabCap <banlazthosow@gmail.com>
2022-10-04 00:19:54 +0300

    created 'banner'

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./banner.scss";
+
+export const Banner = () => {
+	return (
+		<div className="banner">
+			<img src="img/banner.jpg" alt="banner" />
+		</div>
+	);
+};
