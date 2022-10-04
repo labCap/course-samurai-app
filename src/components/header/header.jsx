6b5079ac6e3744d4485728d3197b9b29fdde153c
LabCap <banlazthosow@gmail.com>
2022-10-04 20:07:50 +0300

    Changed the design

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,18 +2,24 @@ import React from "react";
 
 import "./header.scss";
 
-export const Header = ({ openMenu, openingMenu }) => {
+export const Header = () => {
 	return (
 		<header className="header">
 			<div className="container">
 				<div className="header__inner">
 					<div className="ico-logo"></div>
-					<div
+					{/* <div className="header__profile header-profile">
+						<div className="header-profile__logo">
+							<img src="img/user.png" alt="user-logo" />
+						</div>
+					</div>
+					 */}
+					{/* <div
 						className={openMenu ? "ico-menu menu-open" : "ico-menu"}
 						onClick={openingMenu}
 					>
 						<span></span>
-					</div>
+					</div> */}
 				</div>
 			</div>
 		</header>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,23 +4,31 @@ import React from "react";
 
 import "./navbar.scss";
 
-export const NavBar = ({ openMenu }) => {
+export const NavBar = () => {
 	return (
-		<nav className={openMenu ? "nav menu-open" : "nav"}>
+		<nav className="nav">
 			<ul className="nav__list">
 				<li className="nav__list-item active">
+					<div className="profile-ico">
+						<img src="img/user.png" alt="profile img" />
+					</div>
+					{/* <span className="ico-user"></span> */}
 					<a href="#">Profile</a>
 				</li>
 				<li className="nav__list-item">
+					<span className="ico-comment"></span>
 					<a href="#">Messages</a>
 				</li>
 				<li className="nav__list-item">
+					<span className="ico-newspaper"></span>
 					<a href="#">News</a>
 				</li>
 				<li className="nav__list-item">
+					<span className="ico-music"></span>
 					<a href="#">Music</a>
 				</li>
 				<li className="nav__list-item">
+					<span className="ico-gear"></span>
 					<a href="#">Settings</a>
 				</li>
 			</ul>
