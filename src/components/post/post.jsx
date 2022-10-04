94047566567524cda5b94d566e075b7921bf4373
labcap <banlazthosow@gamil.com>
2022-10-04 21:37:28 +0300

    Added logik adding posts

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./post.scss";
 
-export const Post = ({ mess }) => {
+export const Post = ({ mess, img }) => {
 	return (
 		<div className="post">
-			<div className="post__img"></div>
+			<div className="post__img">
+				<img src={img} alt="" />
+			</div>
 			<div className="post__text">{mess}</div>
 		</div>
 	);
--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -4,11 +4,36 @@ import { Post } from "../post/post";
 
 import "./posts.scss";
 
+const userImg = "img/user.png";
+
+const POSTS = [
+	{ mess: "hi", img: userImg },
+	{
+		mess: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, maiores!",
+		img: userImg,
+	},
+	{
+		mess: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+		img: userImg,
+	},
+	{
+		mess: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id odio, numquam assumenda tempora libero sapiente dolor, nihil eaque ducimus ratione voluptatibus molestiae hic exercitationem. Qui dolor esse optio velit reiciendis!",
+		img: userImg,
+	},
+	{ mess: "Lorem, ipsum dolor.", img: userImg },
+	{
+		mess: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure adipisci impedit facilis natus sequi earum!",
+		img: userImg,
+	},
+];
+
 export const Posts = () => {
 	return (
 		<div className="posts">
 			<h2 className="posts__title title">Posts</h2>
-			<Post mess={"hi"} />
+			{POSTS.map((item) => (
+				<Post mess={item.mess} img={item.img} />
+			))}
 		</div>
 	);
 };
