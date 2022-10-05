91e173eb7ea31b8e86cb597bac4d478d1f3f6081
labcap <banlazthosow@gamil.com>
2022-10-05 23:31:24 +0300

    added style and HTML structure to 'message.jsx'

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import "./App.scss";
 
 import { Header } from "./components/header/header";
 import { NavBar } from "./components/navbar/navbar";
-import { Profile } from "./pages/profile";
+import { Message } from "./pages/message/message";
+import { Profile } from "./pages/profile/profile";
 
 function App() {
 	return (
 		<div className="wrapper">
 			<Header />
 
-			<main className="main">
-				{/* <div className="container"> */}
-				<div className="main__inner container">
-					<NavBar />
-					<section className="content">{/* <Profile /> */}</section>
-				</div>
+			<main className="main container">
+				{/* <div className=""> */}
+				{/* <div className="main__inner container"> */}
+				<NavBar />
+				<section className="content">
+					{/* <Profile /> */}
+					<Message />
+				</section>
+				{/* </div> */}
 				{/* </div> */}
 			</main>
 		</div>
---
+++ src/pages/message/message.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./message.scss";
+
+export const Message = () => {
+	return (
+		<div className="message">
+			<ul className="message__list message-list">
+				<li className="message-list__item">
+					<div className="message-list__img"></div>
+					<div className="message-list__name">Viv</div>
+					<span className="message-list__num-mark">1</span>
+				</li>
+			</ul>
+			<div className="message__chat message-chat">
+				<div className="message-chat__header">
+					<button className="message-chat__btn ico-arrow-left"></button>
+					<div className="message-chat__name">Viv</div>
+					<button className="message-chat__settings ico-gear"></button>
+				</div>
+
+				<div className="message-chat__body">
+					<div className="message-chat__user">
+						<div className="message-chat__img"></div>
+						<div className="message-chat__text">hi</div>
+					</div>
+					<div className="message-chat__user message-chat__user-you">
+						<div className="message-chat__img"></div>
+						<div className="message-chat__text">hi</div>
+					</div>
+				</div>
+
+				<div className="message-chat__footer">
+					<input
+						className="message-chat__input input"
+						// name="chat-textarea"
+						// id="chat-textarea"
+					></input>
+					<button
+						className="message-chat__btn ico-arrow-left"
+						type="submit"
+					></button>
+				</div>
+			</div>
+		</div>
+	);
+};
