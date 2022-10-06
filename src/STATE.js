b3d78a4d84b2b317aaf06acc51a282b1dc6ced07
labcap <banlazthosow@gamil.com>
2022-10-06 15:36:19 +0300

    exported the arrays to the state file

---
+++ src/STATE.js
@@ -0,0 +1,45 @@
+const userImg = "img/user.png";
+
+export const POSTS = [
+	{
+		userLogo: userImg,
+		mess: "hi",
+		img: "",
+	},
+	{
+		userLogo: userImg,
+		mess: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, maiores!",
+		img: "img/post-img/photo_2022-09-17_21-06-38.jpg",
+	},
+	{
+		userLogo: userImg,
+		mess: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+		img: "img/post-img/photo_2022-10-05_23-38-59.jpg",
+	},
+	{
+		userLogo: userImg,
+		mess: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id odio, numquam assumenda tempora libero sapiente dolor, nihil eaque ducimus ratione voluptatibus molestiae hic exercitationem. Qui dolor esse optio velit reiciendis!",
+		img: "img/post-img/photo_2022-08-23_14-26-36.jpg",
+	},
+	{
+		userLogo: userImg,
+		mess: "Lorem, ipsum dolor.",
+		img: "",
+	},
+	{
+		userLogo: userImg,
+		mess: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure adipisci impedit facilis natus sequi earum!",
+		img: "",
+	},
+];
+
+export const DIALOG = [
+	{
+		id: 1,
+		img: null,
+		name: "Viv",
+		mess: { u1: "hi", u2: "hy" },
+	},
+	{ id: 2, img: "img/user.png", name: "LCap", mess: { u1: "hi", u2: "hi" } },
+	{ id: 3, img: null, name: "admin", mess: { u1: "hi", u2: "hi" } },
+];
--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -1,43 +1,9 @@
 import React from "react";
+import { POSTS } from "../../STATE.js";
 
 import { Post } from "../post/post";
 
 import "./posts.scss";
-
-const userImg = "img/user.png";
-
-const POSTS = [
-	{
-		userLogo: userImg,
-		mess: "hi",
-		img: "",
-	},
-	{
-		userLogo: userImg,
-		mess: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, maiores!",
-		img: "img/post-img/photo_2022-09-17_21-06-38.jpg",
-	},
-	{
-		userLogo: userImg,
-		mess: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-		img: "img/post-img/photo_2022-10-05_23-38-59.jpg",
-	},
-	{
-		userLogo: userImg,
-		mess: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id odio, numquam assumenda tempora libero sapiente dolor, nihil eaque ducimus ratione voluptatibus molestiae hic exercitationem. Qui dolor esse optio velit reiciendis!",
-		img: "img/post-img/photo_2022-08-23_14-26-36.jpg",
-	},
-	{
-		userLogo: userImg,
-		mess: "Lorem, ipsum dolor.",
-		img: "",
-	},
-	{
-		userLogo: userImg,
-		mess: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure adipisci impedit facilis natus sequi earum!",
-		img: "",
-	},
-];
 
 export const Posts = () => {
 	return (
